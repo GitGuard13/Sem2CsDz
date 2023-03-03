637542e3017e4fb3ee5c7764731ea516674a4a16
Your Name <nagimov2016@inbox.ru>
2023-03-04 02:07:08 +0300

    Dz2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+// #10
+
+// Console.Write("Введите число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+
+// if (Number > 99 && Number < 1000) 
+//     {
+//     int result = ((Number / 10) % 10);
+//         Console.WriteLine($"{Number} -> {result}");
+//     }
+//     else
+//        Console.WriteLine("Неверное число");
+
+// #13
+
+// Console.Write("Введите число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+
+// if (Number > 9999 && Number < 100000) 
+// {
+//     int result = ((Number / 100) % 10);
+//     Console.WriteLine($"{Number} -> {result}");
+// }
+// if (Number < 100)
+//     Console.WriteLine($"{Number} -> Третьего числа нет");
+
+// #15
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n >= 1 && n <= 7)
+// {
+//     if (n <= 5)
+//         Console.WriteLine($"{n} -> Будний день");
+//     if (n > 5 && n <= 7)
+//         Console.WriteLine($"{n} -> Выходной день");    
+// }
